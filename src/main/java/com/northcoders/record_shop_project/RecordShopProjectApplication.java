93c6f2f0999415c682a9c732e3b8dfcc54a7a88a
Hollie <holliemount11@hotmail.com>
2024-12-05 11:11:16 +0000

    First commit

---
+++ src/main/java/com/northcoders/record_shop_project/RecordShopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.record_shop_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/record_shop_project/RecordShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.record_shop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
