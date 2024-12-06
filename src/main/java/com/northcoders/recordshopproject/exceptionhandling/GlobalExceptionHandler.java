97cb74bf359c092e75233080ad868cd22562ff77
Hollie <holliemount11@hotmail.com>
2024-12-06 12:01:22 +0000

    Feat: add GlobalExceptionHandler class

---
+++ src/main/java/com/northcoders/recordshopproject/exceptionhandling/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.northcoders.recordshopproject.exceptionhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
